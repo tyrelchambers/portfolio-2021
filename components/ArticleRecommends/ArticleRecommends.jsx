1043ca6b502012a3f1a297dd1d87bab001349e78
Tyrel Chambers <tychambers3@gmail.com>
2022-02-19 16:21:16 -0500

    .

--- components/ArticleRecommends/ArticleRecommends.jsx
+++ components/ArticleRecommends/ArticleRecommends.jsx
@@ -12,47 +12,48 @@ const ArticleRecommends = ({ articles }) => {
     }
 
     setState([articles.at(articleIndex1), articles.at(articleIndex2)]);
-  }, []);
+  }, [articles]);
 
   return (
     <section className="max-w-screen-lg mt-8 w-full ml-auto mr-auto ">
       <p className="mb-4 text-lg font-bold">You might also enjoy...</p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {state
-          .filter((p) =>
-            process.env.NODE_ENV !== "development"
-              ? p.frontmatter?.published !== false
-              : p
-          )
+        {state.length > 0 &&
+          state
+            .filter((p) =>
+              process.env.NODE_ENV !== "development"
+                ? p.frontmatter.published !== false
+                : p
+            )
 
-          .map(({ frontmatter, slug }) => (
-            <div
-              className="bg-gray-100 p-4 rounded-lg flex flex-col sm:flex-row"
-              key={frontmatter.title}
-            >
-              {console.log("frontmatter in artcile recommends", frontmatter)}
-              {frontmatter.banner && (
-                <img
-                  src={frontmatter.banner}
-                  alt=""
-                  className="w-48 rounded-lg mr-6 shadow-md object-cover article-thumb"
-                />
-              )}
+            .map(({ frontmatter, slug }) => (
+              <div
+                className="bg-gray-100 p-4 rounded-lg flex flex-col sm:flex-row"
+                key={frontmatter.title}
+              >
+                {console.log("frontmatter in artcile recommends", frontmatter)}
+                {frontmatter.banner && (
+                  <img
+                    src={frontmatter.banner}
+                    alt=""
+                    className="w-48 rounded-lg mr-6 shadow-md object-cover article-thumb"
+                  />
+                )}
 
-              <main className="flex flex-col">
-                <header className="flex items-center gap-6">
-                  <h2>
-                    <Link href={"/blog/[slug]"} as={`/blog/${slug}`}>
-                      <a className="text-lg font-bold text-gray-800 hover:text-blue-500 transition-all">
-                        {frontmatter.title}
-                      </a>
-                    </Link>
-                  </h2>
-                </header>
-              </main>
-            </div>
-          ))}
+                <main className="flex flex-col">
+                  <header className="flex items-center gap-6">
+                    <h2>
+                      <Link href={"/blog/[slug]"} as={`/blog/${slug}`}>
+                        <a className="text-lg font-bold text-gray-800 hover:text-blue-500 transition-all">
+                          {frontmatter.title}
+                        </a>
+                      </Link>
+                    </h2>
+                  </header>
+                </main>
+              </div>
+            ))}
 
         <style jsx>{`
           .article-thumb {
